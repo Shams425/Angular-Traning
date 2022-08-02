1f6d032dd5df2a86b0919c7f81ba901dac51214e
Shams425 <alexdavid486@gmail.com>
2022-08-02 19:36:15 +0200

    making a directive for hight light an element

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HighLightDirective } from './high-light.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HighLightDirective
   ],
   imports: [
     BrowserModule,
---
+++ src/app/high-light.directive.ts
@@ -0,0 +1,10 @@
+import { Directive, ElementRef } from '@angular/core';
+
+@Directive({
+  selector: '[appHighLight]',
+})
+export class HighLightDirective {
+  constructor(el: ElementRef) {
+    el.nativeElement.style.backgroundColor = 'red';
+  }
+}
